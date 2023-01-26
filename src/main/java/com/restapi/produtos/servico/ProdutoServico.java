036a3c4b87f0b22eb0225cf623f5bd3c9c261791
LukeMocroski <devmocroski@gmail.com>
2023-01-26 11:22:11 -0300

    Criando servico para cadastrar produtos

--- src/main/java/com/restapi/produtos/servico/ProdutoServico.java
+++ src/main/java/com/restapi/produtos/servico/ProdutoServico.java
@@ -1,9 +1,12 @@
 package com.restapi.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.restapi.produtos.modelo.ProdutoModelo;
+import com.restapi.produtos.modelo.RespostaModelo;
 import com.restapi.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,8 +15,27 @@ public class ProdutoServico {
 	@Autowired
 	private ProdutoRepositorio pr;
 	
+	@Autowired
+	private RespostaModelo rm;
+	
 	//metodo para listar produtos
 	public Iterable<ProdutoModelo> listar(){
 		return pr.findAll();
 	}
+	
+	
+	//metodo para cadastrar produtos
+	public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+		
+		if(pm.getNome().equals("")) {
+			rm.setMensagem("O nome do produto é obrigatório");
+			return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+		}else if(pm.getMarca().equals("")) {
+			rm.setMensagem("O nome da marca é obrigátorio!");
+			return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+		}else {
+			return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+		}
+	}
+	
 }
