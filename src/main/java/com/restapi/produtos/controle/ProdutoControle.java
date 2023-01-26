2d6445ab5acf0d00085a88286d8f2784befd3a35
LukeMocroski <devmocroski@gmail.com>
2023-01-26 11:34:32 -0300

    Rota para o servico de cadastrar produtos

--- src/main/java/com/restapi/produtos/controle/ProdutoControle.java
+++ src/main/java/com/restapi/produtos/controle/ProdutoControle.java
@@ -1,7 +1,10 @@
 package com.restapi.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restapi.produtos.modelo.ProdutoModelo;
@@ -21,5 +24,10 @@ public class ProdutoControle {
 	@GetMapping("/")
 	public String rota() {
 		return "API Funcionando";
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+		return ps.cadastrar(pm);
 	}
 }
