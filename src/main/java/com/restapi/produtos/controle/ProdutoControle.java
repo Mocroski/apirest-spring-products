1a367c1398ad11f2419bccb0f7bb5344f60aee13
LukeMocroski <devmocroski@gmail.com>
2023-01-27 12:56:35 -0300

    Criando a rota para o metodo de editar

--- src/main/java/com/restapi/produtos/controle/ProdutoControle.java
+++ src/main/java/com/restapi/produtos/controle/ProdutoControle.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +27,13 @@ public class ProdutoControle {
 		return "API Funcionando";
 	}
 	
-	@PostMapping("/cadastrarAlterar")
+	@PostMapping("/cadastrar")
 	public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
 		return ps.cadastrarAlterar(pm, "cadastrar");
+	}
+	
+	@PutMapping("/alterar")
+	public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+		return ps.cadastrarAlterar(pm, "alterar");
 	}
 }
